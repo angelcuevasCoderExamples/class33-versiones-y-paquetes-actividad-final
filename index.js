e53d973b3c31219cfbf1466462e7247eca21d80c
angelcuevas <angel.p.cuevas@gmail.com>
2024-04-30 22:40:50 -0300

    added: math module for npm

---
+++ index.js
@@ -0,0 +1,19 @@
+function sumar(numero1, numero2){
+    return numero1 + numero2; 
+}
+function restar(numero1, numero2){
+    return numero1 - numero2; 
+}
+function multiplicar(numero1, numero2){
+    return numero1 * numero2; 
+}
+function dividir(numero1, numero2){
+    return numero1/numero2; 
+}
+
+module.exports = {
+    sumar, 
+    restar,
+    multiplicar,
+    dividir 
+}
